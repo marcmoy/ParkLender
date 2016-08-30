6d4ddbd84660f83bf881b7e5bbe6d2b4b2c146f8
Marc Moy <moymarc453@gmail.com>
2016-08-30 11:09:23 -0700

    root view works

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,10 @@ ActiveRecord::Schema.define(version: 20160830172549) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
-    t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'marc', password: 'password')
+User.create(username: 'demo-user', password: 'password')
