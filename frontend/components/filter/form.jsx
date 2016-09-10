e47c287f4dfd2fd63f7a801ae7f3724fd6dec8d6
Marc Moy <moymarc453@gmail.com>
2016-09-10 12:06:09 -0700

    added slider and fixed some styles

--- frontend/components/filter/form.jsx
+++ frontend/components/filter/form.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 import PriceType from './input_fields/price_type';
 import DateInput from './input_fields/date_input';
 import TimeRange from './input_fields/time_range';
+import Rheostat from 'rheostat';
 
 const handleChange = (filter, updateFilter) => (
   e => updateFilter(filter, e.currentTarget.value)
@@ -13,6 +14,11 @@ const FilterForm = ({filter, updateFilter, updateMap}) => (
   <form className='filter-form-container'>
     <div className='form-group'>
       <PriceType filter={filter} updateFilter={updateFilter} />
+    </div>
+    <div className='form-group'>
+      <div className='row rheostat-row'>
+        <Rheostat min={1} max={100} values={[1,100]} />
+      </div>
     </div>
   </form>
 );
