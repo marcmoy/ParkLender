4a8d2f14c3b598299cf03e2f4dd8aaf78663a6e6
Marc Moy <moymarc453@gmail.com>
2016-10-03 20:39:43 -0700

    fixed splash image and added rough how it works component

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slideshow from './slideshow';
+import HowItWorks from './how_it_works';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -10,6 +11,10 @@ class HomePage extends React.Component {
     return (
       <div>
         <Slideshow updateMap={this.props.updateMap}/>
+        <HowItWorks />
+        <footer>
+          Here is the footer
+        </footer>
       </div>
     );
   }
---
+++ frontend/components/home/how_it_works.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class HowItWorks extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='how-it-works-container row'>
+        <h1>How it works</h1>
+        <p>
+          blurbs
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HowItWorks;
